4a75ff4cf244339208b314dc886b9da49b787dd5
ogazboiz <ogazboizakpolo@gmail.com>
2023-01-09 08:53:48 +0100

    done

--- script.js
+++ script.js
@@ -6,5 +6,15 @@ let overlay = document.querySelector('.overlay');
 for (let index = 0; index < showModal.length; index++) {
   showModal[index].addEventListener('click', function () {
     modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
   });
 }
+
+closeModal.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+overlay.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
